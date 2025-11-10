780b9367a954122769c97e6c8ccbe8f2a75bea54
prankstah20 <prankstah20@gmail.com>
2025-11-10 01:22:41 -0500

    changed saveblock space to expand bag size

--- include/constants/global.h
+++ include/constants/global.h
@@ -77,7 +77,7 @@
 #define ROAMER_COUNT 1 // Number of maximum concurrent active roamers
 
 // Bag constants
-#define BAG_ITEMS_COUNT 30
+#define BAG_ITEMS_COUNT 99
 #define BAG_KEYITEMS_COUNT 30
 #define BAG_POKEBALLS_COUNT 16
 #define BAG_TMHM_COUNT 64
